6391bd2765d4006488bdaa0f0d13a6bece23ec31
Bilel Hammemi <bilel.hammami@insat.ucar.tn>
2025-05-02 21:03:20 +0100

    this is updated on 2103

---
+++ src/Controller/FirstController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class FirstController extends AbstractController
+{
+    #[Route('/first', name: 'first')]
+    public function index(): Response
+    {
+
+        return $this->render('first/index.html.twig',[
+            'name'=> 'bilel',
+            'firstname' => 'hammemi']);
+    }
+
+    #[route('/sayHello', name: 'say.hello')]
+    public function sayHello(): Response{
+        $x=rand(0,2);
+        echo $x;
+        if($x==1){
+        return $this->forward('App\Controller\FirstController::index');}
+        return $this->render('first/sayHello.html.twig',[]);
+    }
+    }
